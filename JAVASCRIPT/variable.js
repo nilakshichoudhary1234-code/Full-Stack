82af00c2709dc9ded37f39a7bc9c143a7bc69b64
Nilakshi Choudhary <nilakshichoudhary1234@gmail.com>
2026-02-03 23:46:19 +0530

    basic

---
+++ JAVASCRIPT/variable.js
@@ -0,0 +1,22 @@
+//variables 
+
+let name ="nilakshi";//in let we can change the value 
+let age=19;
+name="nikhil";
+
+console.log(name , age);
+
+const account = 5247;// in case of the const we cannot change the value 
+//account = 23;
+
+console.log(account);
+
+//old method:
+var a=10; // the disadvantage of var is that it can assign multiple values to the same variable at the same time.
+var a=20;
+console.log(a);
+
+
+
+
+
