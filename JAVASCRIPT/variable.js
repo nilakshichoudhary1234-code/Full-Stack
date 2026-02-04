3dcda878d7de0fe5a1864c1a7bef0d6ad1f740e1
Nilakshi Choudhary <nilakshichoudhary1234@gmail.com>
2026-02-04 23:47:23 +0530

    variables

--- JAVASCRIPT/variable.js
+++ JAVASCRIPT/variable.js
@@ -17,6 +17,85 @@ var a=20;
 console.log(a);
 
 
+//about datatypes:
+//primpitive datatype:number , string , boolean , undefined , null,bidigit,symbol
+//number:
+let b= 5;
+let c=10;
+console.log(b,c);
+console.log(typeof a);
+
+
+//string
+let d="pari";
+let e='javascript';
+console.log(typeof d);
+console.log(d,e);
+
+//boolean
+let login=true;
+console.log(login);
+
+//undefined
+let user;
+console.log(user);
+
+//bigint
+let num=1458731874n;
+console.log(num);
+
+//null
+let weather = null;
+console.log(weather);
+
+//symbol
+const id1=Symbol("id");
+const id2=Symbol("id");
+console.log(id1==id2);
+
+//non primitive datatype: array , object , function 
+//array: it is the object type.
+let arr=[10,30,50,"nilli",false];
+console.log(arr);
+
+//object present in key value
+let obj={
+    name:"rohit",
+    age:18,
+    account : 47512,
+    category: "general"
+
+}
+console.log(obj);
+
+ let s=function add(){
+    console.log("hello javascript");
+    
+}
+//add();
+//s();
+console.log(s);
+
+//primitivre datatype is immutable.
+let n=10;
+n=20;
+console.log(a);
+
+let str="mohit";
+str="sohit";
+console.log(str);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
