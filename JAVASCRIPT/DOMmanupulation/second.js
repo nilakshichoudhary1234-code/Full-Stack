9e38de7a3f1cc67dc146e51d6de09ddfe0cfdeea
Nilakshi Choudhary <nilakshichoudhary1234@gmail.com>
2026-02-22 22:45:09 +0530

    today work

---
+++ JAVASCRIPT/DOMmanupulation/second.js
@@ -0,0 +1,16 @@
+// function handleClick(){
+//     const element = document.getElementById("first");
+//     element.textContent = "Strike is coming";
+
+// }
+
+//id="first" onclick="handleClick()" -- on h1 in html file.
+// document.getElementById("first");
+// Element.onclick = function handleClick(){
+//     Element.textContent = "Strike is coming soon";
+
+// }  // this is not used because when we want to more than one click on the same then it will skip the indermediate one.
+
+Element.addEventListener('click',()=>{
+    
+})
