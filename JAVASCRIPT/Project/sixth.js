cc05c34bce42ba3efc86a3be2675f6c5af0b8612
Nilakshi Choudhary <nilakshichoudhary1234@gmail.com>
2026-02-28 09:16:56 +0530

    project

--- JAVASCRIPT/Project/sixth.js
+++ JAVASCRIPT/Project/sixth.js
@@ -1,5 +1,22 @@
-const result = document.getElementById('result');
+//const result = document.getElementById('result');
 //millisecond
-const olympicTime = new Date(2028, 6, 14).getTime();
-
 const current = Date.now();
+const olympicTime = new Date(2028, 6, 14).getTime();
+let timer = olympicTime - currentTime;
+
+const day = Math.floor((timer) / (1000 * 60 * 60 * 24));
+timer %= 1000 * 60 * 60 * 24;
+const hour = Math.floor(timer / (1000 * 60 * 60 ));
+timer %= 1000 * 60 * 60 ;
+
+const minute = Math.floor(timer / (1000 * 60 ));
+timer %= 1000 * 60 ;
+
+const second = Math.floor(timer / (1000 ));
+timer %= 1000;
+
+result.textContent = `Days ${day} Hour : ${hour} Minute: ${minute} Second : ${second}`
+
+
+
+
