f382c8f2b5d92f8a269fde1c61819ec80214e109
Nilakshi Choudhary <nilakshichoudhary1234@gmail.com>
2026-03-01 21:39:52 +0530

    project

--- JAVASCRIPT/Project/sixth.js
+++ JAVASCRIPT/Project/sixth.js
@@ -1,22 +1,23 @@
-//const result = document.getElementById('result');
-//millisecond
-const current = Date.now();
-const olympicTime = new Date(2028, 6, 14).getTime();
-let timer = olympicTime - currentTime;
+setInterval(() => {
+  const result = document.getElementById("result");
+  //millisecond
+  const currentTime = Date.now();
+  const olympicTime = new Date(2028, 6, 14).getTime();
+  let timer = olympicTime - currentTime;
 
-const day = Math.floor((timer) / (1000 * 60 * 60 * 24));
-timer %= 1000 * 60 * 60 * 24;
-const hour = Math.floor(timer / (1000 * 60 * 60 ));
-timer %= 1000 * 60 * 60 ;
+  const day = Math.floor(timer / (1000 * 60 * 60 * 24));
+  timer %= 1000 * 60 * 60 * 24;
 
-const minute = Math.floor(timer / (1000 * 60 ));
-timer %= 1000 * 60 ;
+  const hour = Math.floor(timer / (1000 * 60 * 60));
+  timer %= 1000 * 60 * 60;
 
-const second = Math.floor(timer / (1000 ));
-timer %= 1000;
+  const minute = Math.floor(timer / (1000 * 60));
+  timer %= 1000 * 60;
 
-result.textContent = `Days ${day} Hour : ${hour} Minute: ${minute} Second : ${second}`
+  const second = Math.floor(timer / 1000);
+  timer %= 1000;
 
-
+  result.textContent = ` ${day}:Days ${hour}:Hour  ${minute}:Minute  ${second}:Second `;
+},1000);
 
 
